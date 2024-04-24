78a3aec0fe72bf785dc4511f666648f03c312f27
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-04-23 21:13:32 -0700

    js format

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,38 @@
 // API endpoint
-let apiUrl = 'https://icanhazdadjoke.com/';
+let apiUrl = "https://icanhazdadjoke.com/";
 
 //  random joke
 function fetchJoke() {
   return fetch(apiUrl, {
     headers: {
-      'Accept': 'application/json'
-    }
+      Accept: "application/json",
+    },
   })
-  .then(response => {
-    return response.json();
-  })
-  .then(jokeData => {
-    return jokeData.joke;
-  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((jokeData) => {
+      return jokeData.joke;
+    });
 }
 
 // Function to update the display
 function updateJokeDisplay(joke) {
-  let jokeDisplay = document.getElementById('jokeDisplay');
+  let jokeDisplay = document.getElementById("jokeDisplay");
   if (jokeDisplay) {
     jokeDisplay.textContent = joke;
   }
 }
 
 //  event listener
-let getJokeButton = document.getElementById('getJokeButton');
+let getJokeButton = document.getElementById("getJokeButton");
 if (getJokeButton) {
-  getJokeButton.addEventListener('click', function() {
-    fetchJoke().then(joke => {
+  getJokeButton.addEventListener("click", function () {
+    fetchJoke().then((joke) => {
       if (joke) {
         updateJokeDisplay(joke);
       } else {
-        updateJokeDisplay('Failed to fetch joke.');
+        updateJokeDisplay("Failed to fetch joke.");
       }
     });
   });
